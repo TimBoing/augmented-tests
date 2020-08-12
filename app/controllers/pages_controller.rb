a2ab860c71d43a4684cbacfc55f430e0f00f0b6e
Timothee Boingneres <timboing@icloud.com>
2020-08-12 11:38:52 +0200

    cloudinary

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,17 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
+  skip_before_action :authenticate_user!
   def home
+    # file_path = "../../../Desktop/Blender/objets/robot_project_texture_paint.glb"
+    # Cloudinary::Uploader.upload(file_path, use_filename: true, resource_type: :raw)
   end
+
+  def planet
+  end
+
+  def robot
+  end
+
+  def island
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/island", to: 'pages#island'
+  get "/robot", to: 'pages#robot'
+  get "/planet", to: 'pages#planet'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
