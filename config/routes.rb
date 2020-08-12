33be81af9aa4cf8a9f9e07911f09ad5423bea718
Timothee Boingneres <timboing@icloud.com>
2020-08-12 11:53:01 +0200

    bottle

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/island", to: 'pages#island'
   get "/robot", to: 'pages#robot'
   get "/planet", to: 'pages#planet'
+  get "/bottle", to: 'bottle#planet'
   get "/uploader", to: 'pages#uploader'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
